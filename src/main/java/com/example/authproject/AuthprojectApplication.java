1dd4d6d10ecaf69698c0c14addc3fe4b21de3f3c
Sakshi Deo <deo_s@xoriant.com>
2025-03-04 15:30:25 +0530

    Github authentication

---
+++ src/main/java/com/example/authproject/AuthprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.authproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/authproject/AuthprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.authproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
